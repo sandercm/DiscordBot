9072d9dfedb9ca45e294ef10df32fbb260225b02
Sander <sander.cm97@gmail.com>
2017-06-10 19:52:31 +0200

    added mastery

--- MainBotFiles/Main.py
+++ MainBotFiles/Main.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 import random
+from riotwatcher import RiotWatcher
+from riotwatcher import EUROPE_WEST
+import requests
+import json
+
+
+euw = RiotWatcher('RGAPI-ff834936-b418-4711-8042-e81b804a8441', default_region=EUROPE_WEST)
 
 description = '''Me making a bot because I don't want to study normally.'''
 bot = commands.Bot(command_prefix='?', description=description)
@@ -13,9 +20,18 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+@bot.command()
+async def mastery(user : str, champ : str):
+    user_id = euw.get_summoner(name=user).get('id')
+    champName = champ[0].upper() + champ[1:].lower()
+    champInfo = euw.static_get_champion_list().get('data').get(champName)
+    dictJson = requests.get("https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/" + str(user_id) + "/by-champion/" + str(champInfo.get('id')) + "?api_key=RGAPI-ff834936-b418-4711-8042-e81b804a8441")
+    await bot.say(json.loads(dictJson.content.decode("utf-8")).get("championLevel"))
+
 
 @bot.command()
-async def
+async def sendNudes1():
+    await bot.say("https://images.discordapp.net/attachments/319527831837343746/323150297646104576/XayahxRiven.jpg?width=313&height=407")
 
 
 @bot.command()
